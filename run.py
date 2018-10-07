ceb4bb9c91f2534e726df67be471b858b088d384
M-in <dependence.letgo@gmail.com>
2018-10-07 11:23:45 +0800

    Using Connexion to Add a REST API Endpoint

--- run.py
+++ run.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import connexion
+from flask import render_template
 
-from flask import (Flask, render_template)
+# Create the application instance
+app = connexion.App(__name__, specification_dir='./')
 
-app = Flask(__name__, template_folder="templates")
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
 
 
 @app.route('/')
 def home():
+    """
+    This function just responds to the browser ULR
+    localhost:8000/
+    :return:    the rendered template 'home.html'
+    """
     return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
